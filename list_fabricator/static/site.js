42a8853fe19459d708014d8c3b60b2345934f684
VanguardAWS <ethanturner93@hotmail.com>
2022-12-14 19:36:52 -0600

    added logic to edit weapon loadouts, tally unit points and total points.  added logic to complete list.

--- list_fabricator/static/site.js
+++ list_fabricator/static/site.js
@@ -6,6 +6,9 @@ let imperialKnightsUnits = new Vue({
     imperialKnightsWargearList: [],
     armyList: [],
     pointTotal: 0,
+    finalPointTotal: null,
+    completedArmyList: null,
+    listComplete: false
     },
     methods: {
     getImperialKnightsUnits: function() {
@@ -22,14 +25,40 @@ let imperialKnightsUnits = new Vue({
             
         }).then(res => this.imperialKnightsWargearList = res.data)
     },       
-    addUnit: function(index) {
-        let currentUnit = this.imperialKnightsUnitsList[index]
-        this.pointTotal = currentUnit.point_value + this.pointTotal
-        this.armyList.push(currentUnit)
+    addUnit: function(unit) {
+        this.pointTotal = unit.point_value + this.pointTotal
+        unitCopy = JSON.parse(JSON.stringify(unit))
+        this.armyList.push(unitCopy)
     },
-    removeUnit: function(unit){
-        let currentUnit = this.imperialKnightsUnitsList
-        this.armyList.splice(currentUnit, 1)
+    removeUnit: function(index){
+        let currentUnit = this.armyList[index]
+        this.pointTotal = this.pointTotal - currentUnit.point_value
+        this.armyList.splice(index, 1)
+    },
+    addWeapon: function(unit, weapon, wargear, weaponindex) {
+        this.pointTotal = wargear.point_value + this.pointTotal
+        unit.point_value = unit.point_value + wargear.point_value
+        unit.wargear.push(weapon)
+        unit.optional_wargear.splice(weaponindex, 0) 
+    },
+    removeWeapon: function(unit, index, wargear, wargearindex) {
+        this.pointTotal = this.pointTotal - wargear.point_value
+        unit.point_value = unit.point_value - wargear.point_value
+        this.armyList[index].wargear.splice(wargearindex, 1)
+    },
+    finishList: function(){
+        this.completedArmyList = this.armyList
+        this.finalPointTotal = this.pointTotal
+        this.armyList = null
+        this.pointTotal = null
+        this.listComplete = true
+    },
+    editList: function(){
+        this.armyList = this.completedArmyList
+        this.pointTotal = this.finalPointTotal
+        this.completedArmyList = null
+        this.finalPointTotal = null
+        this.listComplete = false
     }
     },
     beforeMount: function() {
